c7595e7f44cc5800dbd9afadaa942d33dc2515cd
Luo Chunhui <wo@luochunhui.com>
2015-09-26 16:30:54 +0800

    remove unused codes

--- DependencyInjection/GikoAliyunOssExtension.php
+++ DependencyInjection/GikoAliyunOssExtension.php
@@ -27,48 +27,4 @@ class GikoAliyunOssExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
-
-//     public function prepend(ContainerBuilder $container)
-//     {
-//         // get all bundles
-//         $bundles = $container->getParameter('kernel.bundles');
-//         // determine if AcmeGoodbyeBundle is registered
-//         if (isset($bundles['SonataMediaBundle'])) {
-//             // disable AcmeGoodbyeBundle in bundles
-//             $config = array(
-//                 'use_acme_goodbye' => false
-//             );
-//             foreach ($container->getExtensions() as $name => $extension) {
-//                 echo $name;
-//                 switch ($name) {
-//                     case 'sonata_media':
-//                     case 'giko_aliyun':
-//                         // set use_acme_goodbye to false in the config of
-//                         // acme_something and acme_other note that if the user manually
-//                         // configured use_acme_goodbye to true in the app/config/config.yml
-//                         // then the setting would in the end be true and not false
-//                         $container->prependExtensionConfig($name, $config);
-                        
-//                         print_r($config);
-                        
-//                         break;
-//                 }
-//             }
-//         }
-        
-//         // process the configuration of AcmeHelloExtension
-//         $configs = $container->getExtensionConfig($this->getAlias());
-//         // use the Configuration class to generate a config array with
-//         // the settings "acme_hello"
-//         $config = $this->processConfiguration(new Configuration(), $configs);
-        
-//         // check if entity_manager_name is set in the "acme_hello" configuration
-//         if (isset($config['entity_manager_name'])) {
-//             // prepend the acme_something settings with the entity_manager_name
-//             $config = array(
-//                 'entity_manager_name' => $config['entity_manager_name']
-//             );
-//             $container->prependExtensionConfig('acme_something', $config);
-//         }
-//     }
 }
